33ee13b8d727efcb2ec0417fca0a030efeb69177
fedora <ishmaelmafole@gmail.com>
2023-04-10 21:11:00 +0200

    Styled the page (Login) needs a few touchups on the UI.

--- GatePass_SAAF/Views/LoginView.swift
+++ GatePass_SAAF/Views/LoginView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var fnLogin = ""
+    @State var btnLogin = ""
+    
+    
     var body: some View {
     
         ZStack{
@@ -19,26 +24,83 @@ struct LoginView: View {
                     .frame(width: geo.size.width, height: geo.size.height)
             }
             .edgesIgnoringSafeArea(.all)
+            
+            
+            
+            VStack{
+                HStack{
+                    Image(systemName: "arrowshape.backward.fill")
+                        .foregroundColor(Color(white: 0.9))
+                        .padding( .leading, 50.0)
+                        .padding( .top, 70)
+                        .position(CGPointZero)
+                    Text("    LOGIN")
+                        .fontWeight(.heavy)
+                        
+                        .foregroundColor(Color.white)
+                        .padding( .top, 70)
+                        .padding( .leading, -180)
+                        .position(CGPointZero)
+                  
+                }
+            }
+            
+            VStack{
+                Image("Image")
+                    .resizable(resizingMode: .stretch)
+                    .frame(width:65, height:65)
+                
+                VStack(alignment: .leading){
+                    Text("Force Number")
+                        .foregroundColor(Color(white: 1.0))
+                        .fontWeight(.bold)
+                        .padding(.top, 70)
+                    
+                    HStack{
+                        Image(systemName: "pencil")
+                            .foregroundColor(Color(white: 0.9))
+                        TextField("force no", text: $fnLogin)
+                    }
+                    
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    
+                    Text("Password")
+                        .foregroundColor(Color(white: 1.0))
+                        .fontWeight(.bold)
+                        .padding(.top)
+                    
+                    HStack{
+                        Image(systemName: "book")
+                            .foregroundColor(Color(white: 0.9))
+                        TextField("force no", text: $fnLogin)
+                    }
+                    
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    
+                }
+                .padding()
+            }
+            
+            Button(action: {
+                print(self.$btnLogin)
+            }, label: {
+                
+                Text("Login")
+                    .fontWeight(.heavy)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .frame(width:100, height:30)
+                    .padding(.top, 400.0)
+                    
+            })
         }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
     }
 }
+        
+        
+        
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
