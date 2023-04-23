ab700ec8a50e45269c4c0e02999a3228af1b60be
fedora <ishmaelmafole@gmail.com>
2023-04-23 21:54:32 +0200

    Update on Closing the app

--- GatePass_SAAF/Views/Passed.swift
+++ GatePass_SAAF/Views/Passed.swift
@@ -12,6 +12,8 @@ struct Passed: View {
         
         ZStack{
             
+            
+            
             GeometryReader { geo in
                 Image("MainBK")
                     .resizable()
@@ -19,9 +21,22 @@ struct Passed: View {
                     .frame(width: geo.size.width, height: geo.size.height)
             }
             .edgesIgnoringSafeArea(.all)
-            
+            VStack{
+                VStack {
+                   
+                    Button("X"){
+                
+                          exit(0)
+                    }
+                    .foregroundColor(Color(white: 0.9))
+                    .padding( .leading, 50.0)
+                    .padding( .top, 70)
+                    .position(CGPointZero)
+                }
+            }
             
             VStack{
+                
                 Image("Image")
                     .resizable(resizingMode: .stretch)
                     .frame(width:65, height:65)
